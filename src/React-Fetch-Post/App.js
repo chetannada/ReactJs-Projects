15946b9958d6c3ab7e2e88950e0addb936f701bd
Chetan Nada <call2chetannada@gmail.com>
2024-08-13 23:39:58 +0530

    API Name Changes

--- src/React-Fetch-Post/App.js
+++ src/React-Fetch-Post/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <>
       <h1 className="text-center text-3xl p-5">
-        React HTTP POST Requests for NPM API using Fetch in Different way
+        React HTTP POST Requests for Reqres API using Fetch in Different way
       </h1>
       <div className="my-5 space-y-5">
         <PostRequestHooks />
