b0242ed8175947f62f60b9073b66446c5d895972
Chetan Nada <call2chetannada@gmail.com>
2024-08-28 22:47:50 +0530

    Error Component and Fonts are Added

--- Main.jsx
+++ Main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppLayout from "./src/App/AppLayout";
 import Body from "./src/App/Body";
+import Error from "./src/App/Error";
 
 // call createBrowserRouter for routing different pages
 const appRouter = createBrowserRouter([
   {
     path: "/", // show path for routing
     element: <AppLayout />, // show component for particular path
-    // errorElement: <Error />, // show error component for path is different
+    errorElement: <Error />, // show error component if path is wrong
     children: [
       // show children component for routing
       {
