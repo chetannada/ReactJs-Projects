9e60a5912298aead57fddabf61584e5a44adfd16
Chetan Nada <call2chetannada@gmail.com>
2024-09-03 01:40:19 +0530

    Card Max width Value Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,9 @@ module.exports = {
         176: "48rem",
         180: "52rem",
       },
+      maxWidth: {
+        98: "25rem",
+      },
       margin: {
         13: "52px",
         15: "60px",
