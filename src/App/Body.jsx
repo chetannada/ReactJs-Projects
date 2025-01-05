526f947782754abb7e8bd1544755c8fe740c5ded
Chetan Nada <call2chetannada@gmail.com>
2025-01-05 15:46:33 +0530

    Project Details Changes

--- src/App/Body.jsx
+++ src/App/Body.jsx
@@ -26,7 +26,7 @@ const Body = () => {
                   target="_blank"
                   className="flex flex-row gap-1 justify-center items-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 xsm:px-4 py-2.5 text-center me-2 mb-2 mob:w-44"
                 >
-                  Github Code <FaAnglesRight />
+                  Source Code <FaAnglesRight />
                 </Link>
 
                 <Link
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,33 +1,36 @@
 export const projectDetails = [
   {
     id: 1,
-    title: "React Redux Counter App Project",
+    title: "React Redux Counter App",
     description:
-      "Counter App Project using Redux-toolkit and React-redux with Increment and Decrement by value",
+      "A simple and interactive counter application built with React and Redux. It demonstrates state management with Redux Toolkit, allowing users to increment, decrement, and reset the counter efficiently.",
     codeUrl:
       "https://github.com/chetannada/ReactJs-Projects/tree/main/src/Projects/Redux-Counter-App",
     liveUrl: "/redux-counter-app",
   },
   {
     id: 2,
-    title: "React HTTP GET Request Project",
-    description: "Fetch the NPM API using HTTP Get method in Different way",
+    title: "React HTTP GET Request App",
+    description:
+      "Fetch the NPM API using HTTP Get method in Different way and display the data dynamically.",
     codeUrl:
       "https://github.com/chetannada/ReactJs-Projects/tree/main/src/Projects/React-Fetch-Get",
     liveUrl: "/react-fetch-get",
   },
   {
     id: 3,
-    title: "React HTTP POST Request Project",
-    description: "Fetch the Reqres API using HTTP post method in Different way",
+    title: "React HTTP POST Request App",
+    description:
+      "Fetch the Reqres API using HTTP post method in Different way and display the data dynamically.",
     codeUrl:
       "https://github.com/chetannada/ReactJs-Projects/tree/main/src/Projects/React-Fetch-Post",
     liveUrl: "/react-fetch-post",
   },
   {
     id: 4,
-    title: "React Google Auth Project",
-    description: "Allows to login securely via Google account",
+    title: "React Google Authentication App",
+    description:
+      "A React application that integrates Google Sign-In for secure user authentication.",
     codeUrl:
       "https://github.com/chetannada/ReactJs-Projects/tree/main/src/Projects/React-Google-Auth",
     liveUrl: "/react-google-auth",
