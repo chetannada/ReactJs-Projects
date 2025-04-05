eb8d5fd7f67da1512702968c425a74ceb92ba018
Chetan Nada <call2chetannada@gmail.com>
2025-04-05 14:29:44 +0530

    Mock values added for firebase config console error

--- src/Projects/React-Google-Auth/FirebaseConfig.jsx
+++ src/Projects/React-Google-Auth/FirebaseConfig.jsx
@@ -5,16 +5,19 @@
 4. Replace below firebaseConfig with yours.
 */
 
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_API_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_API_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_API_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_API_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_API_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_API_FIREBASE_API_KEY || "mockApiKey",
+  authDomain: import.meta.env.VITE_API_FIREBASE_AUTH_DOMAIN || "mockAuthDomain",
+  projectId: import.meta.env.VITE_API_FIREBASE_PROJECT_ID || "mockProjectId",
+  storageBucket:
+    import.meta.env.VITE_API_FIREBASE_STORAGE_BUCKET || "mockStorageBucket",
+  messagingSenderId:
+    import.meta.env.VITE_API_FIREBASE_MESSAGING_SENDER_ID ||
+    "mockMessagingSenderId",
+  appId: import.meta.env.VITE_API_FIREBASE_APP_ID || "mockAppId",
 };
 
 const app = initializeApp(firebaseConfig);
