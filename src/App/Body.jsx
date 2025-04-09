7cd14c69725dc7b9fc7d2e7de83703a20569c546
Chetan Nada <call2chetannada@gmail.com>
2025-04-09 23:02:34 +0530

    Github icon added in button

--- src/App/Body.jsx
+++ src/App/Body.jsx
@@ -1,6 +1,7 @@
-import { FaAnglesRight } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { projectDetails } from "../utils/constant";
+import { IoOpenOutline } from "react-icons/io5";
 
 const Body = () => {
   return (
@@ -10,7 +11,7 @@ const Body = () => {
           return (
             <div
               key={index.toString() + 1}
-              className="max-w-sm py-4 px-6 flex flex-col justify-between items-start bg-white hover:shadow-xl border border-gray-200 rounded-tr-3xl rounded-bl-3xl shadow dark:bg-gray-800 dark:border-gray-700"
+              className="max-w-sm py-4 px-6 flex flex-col justify-between items-start bg-opacity-50 bg-purple-50 hover:shadow-xl border border-gray-200 rounded-tr-3xl rounded-bl-3xl shadow dark:bg-gray-800 dark:border-gray-700"
             >
               <h5 className="mb-2 text-2xl font-bold text-gray-900 dark:text-white">
                 {item?.title}
@@ -24,16 +25,17 @@ const Body = () => {
                 <Link
                   to={item?.codeUrl}
                   target="_blank"
-                  className="flex flex-row gap-1 justify-center items-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 xsm:px-4 py-2.5 text-center me-2 mb-2 mob:w-44"
+                  className="flex flex-row gap-2 justify-center items-center text-white bg-gradient-to-br from-fuchsia-500 to-blue-900 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 xsm:px-4 py-2.5 text-center me-2 mb-2 mob:w-44"
                 >
-                  Source Code <FaAnglesRight />
+                  <FaGithub size={20} /> Code
                 </Link>
 
                 <Link
                   to={item?.liveUrl}
-                  className="flex flex-row gap-1 justify-center items-center text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 xsm:px-4 py-2.5 text-center me-2 mb-2 mob:w-44"
+                  className="flex flex-row gap-2 justify-center items-center text-white bg-gradient-to-r from-pink-500 to-purple-700 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 xsm:px-4 py-2.5 text-center me-2 mb-2 mob:w-44"
                 >
-                  Live App <FaAnglesRight />
+                  <IoOpenOutline size={20} />
+                  Live Demo
                 </Link>
               </div>
             </div>
