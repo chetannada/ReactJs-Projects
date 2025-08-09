d5f1a8f8c3ddcce2b49a177cf3d23ba158d365b8
Chetan Nada <call2chetannada@gmail.com>
2025-08-09 22:00:08 +0530

    Error boundary file changes

--- src/App/Error.jsx
+++ src/App/Error.jsx
@@ -1,18 +1,33 @@
 import { Link, useRouteError } from "react-router-dom";
-import errorImage from "../assets/Error-404.png";
 import { FaHome } from "react-icons/fa";
+import { MdErrorOutline } from "react-icons/md";
 
 const Error = () => {
-  // access error data while routing using useRouteError()
   const err = useRouteError();
+
   return (
     <>
       <div className="px-4 py-2 w-full h-screen flex flex-col justify-center items-center gap-3 bg-slate-300">
-        <img src={errorImage} alt="Error Image" className="w-108" />
+        <MdErrorOutline className="w-24 h-24 text-red-500" />
+
         <h1 className="font-bold text-xl uppercase">
-          Sorry, The Page not Found!
+          Oops! Something went wrong.
         </h1>
-        <h3 className="text-red-600">{err.data}</h3>
+
+        {err?.message && (
+          <h3 className="text-red-600">
+            {err?.name && <strong>{err.name}:</strong>} {err?.message}
+          </h3>
+        )}
+
+        {err?.data && <h3 className="text-red-600">{err?.data}</h3>}
+
+        {err?.status && (
+          <p className="text-red-600">
+            Status: {err.status} {err?.statusText}
+          </p>
+        )}
+
         <Link
           to="/"
           className="flex flex-row gap-2 justify-center items-center text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
