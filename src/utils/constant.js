166f95412c64cce9caae1bbcb59f84c81c502111
Chetan Nada <call2chetannada@gmail.com>
2025-08-15 21:18:43 +0530

    Project details file changes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,7 +5,7 @@ export const projectDetails = [
     description:
       "A sleek and intuitive React based to-do app that helps users efficiently organize tasks, prioritize activities, and stay productive.",
     codeUrl:
-      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/Projects/ToDo-List",
+      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/features/todo-list",
     liveUrl: "/todo-list",
     contributor: "Chetan Nada",
     contributorAvatar: "https://avatars.githubusercontent.com/u/83969719?v=4",
@@ -17,7 +17,7 @@ export const projectDetails = [
     description:
       "A simple and interactive counter application built with React and Redux. It demonstrates state management with Redux Toolkit, allowing users to increment, decrement, and reset the counter efficiently.",
     codeUrl:
-      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/Projects/Redux-Counter",
+      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/features/redux-counter",
     liveUrl: "/redux-counter",
     contributor: "Chetan Nada",
     contributorAvatar: "https://avatars.githubusercontent.com/u/83969719?v=4",
@@ -29,7 +29,7 @@ export const projectDetails = [
     description:
       "A React application that integrates Google Sign-In for secure user authentication.",
     codeUrl:
-      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/Projects/Google-Auth",
+      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/features/google-auth",
     liveUrl: "/google-auth",
     contributor: "Kaushik Teraiya",
     contributorAvatar: "https://avatars.githubusercontent.com/u/113781417?v=4",
@@ -41,7 +41,7 @@ export const projectDetails = [
     description:
       "Fetch the NPM API using HTTP Get method in Different way and display the data dynamically.",
     codeUrl:
-      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/Projects/Http-Get-Request",
+      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/features/http-get-request",
     liveUrl: "/http-get-request",
     contributor: "Chetan Nada",
     contributorAvatar: "https://avatars.githubusercontent.com/u/83969719?v=4",
@@ -53,7 +53,7 @@ export const projectDetails = [
     description:
       "Fetch the Reqres API using HTTP post method in Different way and display the data dynamically.",
     codeUrl:
-      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/Projects/Http-Post-Request",
+      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/features/http-post-request",
     liveUrl: "/http-post-request",
     contributor: "Chetan Nada",
     contributorAvatar: "https://avatars.githubusercontent.com/u/83969719?v=4",
@@ -61,11 +61,11 @@ export const projectDetails = [
   },
   {
     id: 6,
-    title: "React Image Generation",
+    title: "Image Generation",
     description:
-      "A React application that allows users to generate images using AI, powered by the Flux API using Hagging Face.",
+      "Application that allows users to generate images using AI, powered by the Flux API using Hagging Face.",
     codeUrl:
-      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/Projects/React-Image-Generation",
+      "https://github.com/chetannada/ReactJs-Projects/tree/main/src/features/image-generation",
     liveUrl: "/image-generation",
     contributor: "Sankalp Haritash",
     contributorAvatar: "https://avatars.githubusercontent.com/u/110713125?v=4",
